19f38cac23637665f6e5921bad0fffca06085782
Z9TONEDEVELOPER <maxalex1018@mail.ru>
2025-11-13 20:17:53 +0300

    Add TitlePage

--- Views/CreatingReportView.xaml.cs
+++ Views/CreatingReportView.xaml.cs
@@ -2,14 +2,18 @@ namespace MauiApp1;
 
 public partial class CreatingReportView : ContentView
 {
-    public CreatingReportView()
+	public CreatingReportView()
+	{
+		InitializeComponent();
+	}
+	private void LoadView(View view)
     {
-        InitializeComponent();
+        ReportContent.Content = view;
     }
 
     private void OpenTitle_Clicked(object sender, EventArgs e)
     {
-        
+        LoadView(new TitlePageView());
     }
 
     private void OpenForm_Clicked(object sender, EventArgs e)
---
+++ Views/TitlePageView.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1;
+
+public partial class TitlePageView : ContentView
+{
+	public TitlePageView()
+	{
+		InitializeComponent();
+	}
+}
